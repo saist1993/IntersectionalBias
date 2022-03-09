5f1c81a6c748d372b550408bb4458a4b0322c3c2
gmaheshwari <gaurav.maheshwari@inria.fr>
2022-03-09 16:54:57 +0100

    in process of writing accuracy parity function.

--- src/utils/fairness_function.py
+++ src/utils/fairness_function.py
@@ -1,4 +1,6 @@
 import numpy as np
+from typing import List
+from itertools import product
 
 
 def create_mask(data, condition):
@@ -35,3 +37,39 @@ def create_mask(data, condition):
     mask = np.logical_and.reduce(mask)
     return mask
 
+
+def create_all_possible_groups(number_of_attributes: int):
+    return [i for i in product([0, 1, 'x'], repeat=number_of_attributes)]
+
+
+def get_gerrymandering_groups(groups: List[tuple]):
+    tuple_to_remove = tuple(['x' for _ in range(len(groups[0]))])
+    try:
+        groups.remove(tuple_to_remove)
+    except ValueError:
+        pass
+    return groups
+
+
+def get_independent_groups(groups: List[tuple]):
+    '''All x's apart from one place'''
+    number_of_attribute = len(groups[0])
+    independent_groups = []
+    for group in groups:
+        if group.count('x') == number_of_attribute - 1:
+            independent_groups.append(group)
+    return independent_groups
+
+
+
+def get_intersectional_groups(groups: List[tuple]):
+    '''No x's in any place'''
+    intersectional_groups = []
+    for group in groups:
+        if group.count('x') == 0:
+            intersectional_groups.append(group)
+    return intersectional_groups
+
+
+def get_accuracy_parity(predictions, labels):
+    pass
--- src/utils/test_fairness_function.py
+++ src/utils/test_fairness_function.py
@@ -1,5 +1,7 @@
-from .fairness_function import create_mask
+import collections
 import numpy as np
+from .fairness_function import create_mask, \
+    create_all_possible_groups, get_gerrymandering_groups, get_independent_groups, get_intersectional_groups
 
 def test_mask_simple_condition_example_1():
     a = np.asarray([[1, 1], [1, 1], [1, 0], [1, 0], [1, 1], [1, 0], [0, 1], [0, 1], [0, 0], [0, 0]])
@@ -31,3 +33,28 @@ def test_mask_no_condtion():
     expected_mask = [True, True, True, True, True, True, True, True, True, True]
     received_mask = create_mask(a, condition)
     assert np.array_equal(expected_mask, received_mask)
+
+
+def test_all_create_all_possible_groups_simple():
+    number_of_attributes = 2
+    expected_results = [(1,1), (1,0), (0,1), (0,0), ('x',1), (1,'x'), ('x','x'), (0,'x'), ('x',0)]
+    received_all_possible_groups = create_all_possible_groups(number_of_attributes=number_of_attributes)
+    assert collections.Counter(expected_results) == collections.Counter(received_all_possible_groups)
+
+def test_get_gerrymandering_groups():
+    groups = [(1,1,1), (2,2,2), ('x','x','x')]
+    assert ('x','x','x') not in get_gerrymandering_groups(groups)
+
+
+def test_get_independent_groups():
+    groups = [(1,1,1), (2,2,2), ('x','x','x'), ('x','x',1), ('x',1,'x')]
+    expected_results = [('x','x',1), ('x',1,'x')]
+    received_groups = get_independent_groups(groups)
+    assert collections.Counter(expected_results) == collections.Counter(received_groups)
+
+
+def test_get_intersectional_groups():
+    groups = [(1,1,1), (2,2,2), ('x','x','x'), ('x','x',1), ('x',1,'x')]
+    expected_results = [(1,1,1), (2,2,2)]
+    received_groups = get_intersectional_groups(groups)
+    assert collections.Counter(expected_results) == collections.Counter(received_groups)
