5f47819fbbab03128d995bb724dc1a97d17b6448
gmaheshwari <gaurav.maheshwari@inria.fr>
2022-08-06 16:31:06 +0200

    some more runs files

---
+++ run_files/titled_adult.sh
@@ -0,0 +1,22 @@
+echo "running methods over adult"
+
+cd ~/codes/IntersectionalBias/src/; python hyperparam_titled_erm.py -seeds 10 -dataset_name adult -batch_size 1000 -model simple_non_linear -epochs 300 -method only_titled_erm -fairness_lambda_s 0.05 -fairness_lambda_e 0.5 -fairness_function equal_opportunity &> log1c &
+cd ~/codes/IntersectionalBias/src/; python hyperparam_titled_erm.py -seeds 20 -dataset_name adult -batch_size 1000 -model simple_non_linear -epochs 300 -method only_titled_erm -fairness_lambda_s 0.05 -fairness_lambda_e 0.5 -fairness_function equal_opportunity &> log2c &
+cd ~/codes/IntersectionalBias/src/; python hyperparam_titled_erm.py -seeds 30 -dataset_name adult -batch_size 1000 -model simple_non_linear -epochs 300 -method only_titled_erm -fairness_lambda_s 0.05 -fairness_lambda_e 0.5 -fairness_function equal_opportunity &> log3c &
+cd ~/codes/IntersectionalBias/src/; python hyperparam_titled_erm.py -seeds 40 -dataset_name adult -batch_size 1000 -model simple_non_linear -epochs 300 -method only_titled_erm -fairness_lambda_s 0.05 -fairness_lambda_e 0.5 -fairness_function equal_opportunity &> log4c &
+cd ~/codes/IntersectionalBias/src/; python hyperparam_titled_erm.py -seeds 50 -dataset_name adult -batch_size 1000 -model simple_non_linear -epochs 300 -method only_titled_erm -fairness_lambda_s 0.05 -fairness_lambda_e 0.5 -fairness_function equal_opportunity &> log5c &
+
+cd ~/codes/IntersectionalBias/src/; python hyperparam_titled_erm.py -seeds 10 -dataset_name adult -batch_size 1000 -model simple_non_linear -epochs 300 -method only_mixup -fairness_lambda_s 0.0 -fairness_lambda_e 0.0 -fairness_function equal_opportunity &> log6c &
+cd ~/codes/IntersectionalBias/src/; python hyperparam_titled_erm.py -seeds 20 -dataset_name adult -batch_size 1000 -model simple_non_linear -epochs 300 -method only_mixup -fairness_lambda_s 0.0 -fairness_lambda_e 0.0 -fairness_function equal_opportunity &> log7c &
+cd ~/codes/IntersectionalBias/src/; python hyperparam_titled_erm.py -seeds 30 -dataset_name adult -batch_size 1000 -model simple_non_linear -epochs 300 -method only_mixup -fairness_lambda_s 0.0 -fairness_lambda_e 0.0 -fairness_function equal_opportunity &> log8c &
+cd ~/codes/IntersectionalBias/src/; python hyperparam_titled_erm.py -seeds 40 -dataset_name adult -batch_size 1000 -model simple_non_linear -epochs 300 -method only_mixup -fairness_lambda_s 0.0 -fairness_lambda_e 0.0 -fairness_function equal_opportunity &> log9c &
+cd ~/codes/IntersectionalBias/src/; python hyperparam_titled_erm.py -seeds 50 -dataset_name adult -batch_size 1000 -model simple_non_linear -epochs 300 -method only_mixup -fairness_lambda_s 0.0 -fairness_lambda_e 0.0 -fairness_function equal_opportunity &> log10c &
+
+cd ~/codes/IntersectionalBias/src/; python hyperparam_titled_erm.py -seeds 10 -dataset_name adult -batch_size 1000 -model simple_non_linear -epochs 300 -method tilted_erm_with_mixup -fairness_lambda_s 0.0 -fairness_lambda_e 0.0 -fairness_function equal_opportunity &> log6 &
+cd ~/codes/IntersectionalBias/src/; python hyperparam_titled_erm.py -seeds 20 -dataset_name adult -batch_size 1000 -model simple_non_linear -epochs 300 -method tilted_erm_with_mixup -fairness_lambda_s 0.0 -fairness_lambda_e 0.0 -fairness_function equal_opportunity &> log7 &
+cd ~/codes/IntersectionalBias/src/; python hyperparam_titled_erm.py -seeds 30 -dataset_name adult -batch_size 1000 -model simple_non_linear -epochs 300 -method tilted_erm_with_mixup -fairness_lambda_s 0.0 -fairness_lambda_e 0.0 -fairness_function equal_opportunity &> log8 &
+cd ~/codes/IntersectionalBias/src/; python hyperparam_titled_erm.py -seeds 40 -dataset_name adult -batch_size 1000 -model simple_non_linear -epochs 300 -method tilted_erm_with_mixup -fairness_lambda_s 0.0 -fairness_lambda_e 0.0 -fairness_function equal_opportunity &> log9 &
+cd ~/codes/IntersectionalBias/src/; python hyperparam_titled_erm.py -seeds 50 -dataset_name adult -batch_size 1000 -model simple_non_linear -epochs 300 -method tilted_erm_with_mixup -fairness_lambda_s 0.0 -fairness_lambda_e 0.0 -fairness_function equal_opportunity &> log10 &
+
+
+wait
---
+++ run_files/titled_erm.sh
@@ -0,0 +1,24 @@
+echo "running methods over adult multi group dataset"
+
+cd ~/codes/IntersectionalBias/src/; python hyperparam_titled_erm.py -seeds 10 -dataset_name adult_multi_group -batch_size 1000 -model simple_non_linear -epochs 300 -method only_titled_erm -fairness_lambda_s 0.05 -fairness_lambda_e 0.5 -fairness_function equal_opportunity &> log1 &
+cd ~/codes/IntersectionalBias/src/; python hyperparam_titled_erm.py -seeds 20 -dataset_name adult_multi_group -batch_size 1000 -model simple_non_linear -epochs 300 -method only_titled_erm -fairness_lambda_s 0.05 -fairness_lambda_e 0.5 -fairness_function equal_opportunity &> log2 &
+cd ~/codes/IntersectionalBias/src/; python hyperparam_titled_erm.py -seeds 30 -dataset_name adult_multi_group -batch_size 1000 -model simple_non_linear -epochs 300 -method only_titled_erm -fairness_lambda_s 0.05 -fairness_lambda_e 0.5 -fairness_function equal_opportunity &> log3 &
+cd ~/codes/IntersectionalBias/src/; python hyperparam_titled_erm.py -seeds 40 -dataset_name adult_multi_group -batch_size 1000 -model simple_non_linear -epochs 300 -method only_titled_erm -fairness_lambda_s 0.05 -fairness_lambda_e 0.5 -fairness_function equal_opportunity &> log4 &
+cd ~/codes/IntersectionalBias/src/; python hyperparam_titled_erm.py -seeds 50 -dataset_name adult_multi_group -batch_size 1000 -model simple_non_linear -epochs 300 -method only_titled_erm -fairness_lambda_s 0.05 -fairness_lambda_e 0.5 -fairness_function equal_opportunity &> log5 &
+
+cd ~/codes/IntersectionalBias/src/; python hyperparam_titled_erm.py -seeds 10 -dataset_name adult_multi_group -batch_size 1000 -model simple_non_linear -epochs 300 -method only_mixup -fairness_lambda_s 0.0 -fairness_lambda_e 0.0 -fairness_function equal_opportunity &> log6 &
+cd ~/codes/IntersectionalBias/src/; python hyperparam_titled_erm.py -seeds 20 -dataset_name adult_multi_group -batch_size 1000 -model simple_non_linear -epochs 300 -method only_mixup -fairness_lambda_s 0.0 -fairness_lambda_e 0.0 -fairness_function equal_opportunity &> log7 &
+cd ~/codes/IntersectionalBias/src/; python hyperparam_titled_erm.py -seeds 30 -dataset_name adult_multi_group -batch_size 1000 -model simple_non_linear -epochs 300 -method only_mixup -fairness_lambda_s 0.0 -fairness_lambda_e 0.0 -fairness_function equal_opportunity &> log8 &
+cd ~/codes/IntersectionalBias/src/; python hyperparam_titled_erm.py -seeds 40 -dataset_name adult_multi_group -batch_size 1000 -model simple_non_linear -epochs 300 -method only_mixup -fairness_lambda_s 0.0 -fairness_lambda_e 0.0 -fairness_function equal_opportunity &> log9 &
+cd ~/codes/IntersectionalBias/src/; python hyperparam_titled_erm.py -seeds 50 -dataset_name adult_multi_group -batch_size 1000 -model simple_non_linear -epochs 300 -method only_mixup -fairness_lambda_s 0.0 -fairness_lambda_e 0.0 -fairness_function equal_opportunity &> log10 &
+
+cd ~/codes/IntersectionalBias/src/; python hyperparam_titled_erm.py -seeds 10 -dataset_name adult_multi_group -batch_size 1000 -model simple_non_linear -epochs 300 -method tilted_erm_with_mixup -fairness_lambda_s 0.0 -fairness_lambda_e 0.0 -fairness_function equal_opportunity &> log6 &
+cd ~/codes/IntersectionalBias/src/; python hyperparam_titled_erm.py -seeds 20 -dataset_name adult_multi_group -batch_size 1000 -model simple_non_linear -epochs 300 -method tilted_erm_with_mixup -fairness_lambda_s 0.0 -fairness_lambda_e 0.0 -fairness_function equal_opportunity &> log7 &
+cd ~/codes/IntersectionalBias/src/; python hyperparam_titled_erm.py -seeds 30 -dataset_name adult_multi_group -batch_size 1000 -model simple_non_linear -epochs 300 -method tilted_erm_with_mixup -fairness_lambda_s 0.0 -fairness_lambda_e 0.0 -fairness_function equal_opportunity &> log8 &
+cd ~/codes/IntersectionalBias/src/; python hyperparam_titled_erm.py -seeds 40 -dataset_name adult_multi_group -batch_size 1000 -model simple_non_linear -epochs 300 -method tilted_erm_with_mixup -fairness_lambda_s 0.0 -fairness_lambda_e 0.0 -fairness_function equal_opportunity &> log9 &
+cd ~/codes/IntersectionalBias/src/; python hyperparam_titled_erm.py -seeds 50 -dataset_name adult_multi_group -batch_size 1000 -model simple_non_linear -epochs 300 -method tilted_erm_with_mixup -fairness_lambda_s 0.0 -fairness_lambda_e 0.0 -fairness_function equal_opportunity &> log10 &
+
+wait
+
+
+
