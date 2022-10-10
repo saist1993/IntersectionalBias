61bde95847311b00b3d2b96f66d0d04a0a46688a
gmaheshwari <gaurav.maheshwari@inria.fr>
2022-10-10 15:11:42 +0200

    added the run to the hyperparam search

--- src/hyperparam_titled_erm.py
+++ src/hyperparam_titled_erm.py
@@ -37,7 +37,7 @@ if __name__ == '__main__':
         titled_scales = [1.0, 5.0, 10.0]
         mixup_scales = [0.3, 0.6, 0.9]
 
-    if args.method == 'only_mixup' or args.method == 'only_mixup_with_loss_group':
+    if args.method == 'only_mixup' or args.method == 'only_mixup_with_loss_group' or args.method == 'train_only_mixup_based_on_distance':
         titled_scales = [0.0]
         mixup_scales = [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9]
 
