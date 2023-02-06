4e70b849bd5457c077c9752abb0e07b952077888
Gaurav Maheshwari <saist1993@gmail.com>
2023-02-06 18:50:14 +0100

    removed groups from twitter

--- src/main.py
+++ src/main.py
@@ -426,7 +426,7 @@ if __name__ == '__main__':
                         default=None)
     parser.add_argument('--dataset_name', '-dataset_name', help="twitter_hate_speech/adult_multi_group/celeb_multigroup_v3",
                         type=str,
-                        default='twitter_hate_speech_augmented')
+                        default='twitter_hate_speech')
 
     parser.add_argument('--log_file_name', '-log_file_name', help="the name of the log file",
                         type=str,
@@ -435,7 +435,7 @@ if __name__ == '__main__':
 
     parser.add_argument('--fairness_function', '-fairness_function', help="fairness function to concern with",
                         type=str,
-                        default='equal_odds')
+                        default='equal_opportunity')
 
     parser.add_argument('--titled_t', '-titled_t', help="fairness function to concern with",
                         type=float,
