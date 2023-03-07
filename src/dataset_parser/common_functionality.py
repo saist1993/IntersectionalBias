15a19a6f8b393c82fa25666a08df405b7465802a
Gaurav Maheshwari <saist1993@gmail.com>
2023-03-07 11:53:54 +0100

    slight debug

--- src/dataset_parser/common_functionality.py
+++ src/dataset_parser/common_functionality.py
@@ -662,11 +662,11 @@ class AugmentData:
         #               ]
         # all_models = pickle.load(open(f"train_and_valid_all_{self.dataset_name.replace('_augmented', '')}.pt", "rb"))
 
-        all_models = [pickle.load(open(f"train_and_valid_all_{self.dataset_name.replace('_augmented', '')}.pt", "rb"))]
+        all_models = [pickle.load(open(f"all_{self.dataset_name.replace('_augmented', '')}.pt", "rb"))]
 
         # classifier_models = pickle.load(open(f"real_vs_fake_{self.dataset_name.replace('_augmented', '')}.sklearn", "rb"))
         classifier_models = pickle.load(
-            open(f"train_and_valid_real_vs_fake_{self.dataset_name.replace('_augmented', '')}.sklearn", "rb"))
+            open(f"real_vs_fake_{self.dataset_name.replace('_augmented', '')}.sklearn", "rb"))
         # classifier_models_2 = pickle.load(open(f"task_classifier_{self.dataset_name.replace('_augmented', '')}.sklearn", "rb"))
         classifier_models_2 = None
 
